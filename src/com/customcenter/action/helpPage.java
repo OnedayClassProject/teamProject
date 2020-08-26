cc328cfe5f49c0ad28fa6df05e0cfc59b6d9d666
reddrinkbro <ghdwn194@naver.com>
2020-08-26 12:44:00 +0900

    helpcomment 기능 추가, 삭제 기능추가

--- src/com/customcenter/action/helpPage.java
+++ src/com/customcenter/action/helpPage.java
@@ -1,6 +1,8 @@
 package com.customcenter.action;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -9,6 +11,8 @@ import javax.servlet.http.HttpServletResponse;
 import com.command.CommandHandler;
 import com.help.db.helpBean;
 import com.help.db.helpDAO;
+import com.helpComment.db.helpCommentBean;
+import com.helpComment.db.helpCommentDAO;
 
 public class helpPage implements CommandHandler{
 
@@ -17,11 +21,22 @@ public class helpPage implements CommandHandler{
 			throws ServletException, IOException {
 		int num = Integer.parseInt(request.getParameter("num"));
 		String pageNum=request.getParameter("pageNum");
+		//게시글
 		helpDAO dao = new helpDAO();
 		helpBean bean = new helpBean();
 		bean = dao.getHelpConent(num);
+		
+		//덧글
+		helpCommentDAO cdao = new helpCommentDAO();
+		List<helpCommentBean> list = new ArrayList<helpCommentBean>();
+		list = cdao.getComment(num);
+		
+		int count = cdao.commentCount(num);
+		
 		request.setAttribute("bean", bean);
 		request.setAttribute("pageNum", pageNum);
+		request.setAttribute("list", list);
+		request.setAttribute("count", count);
 		return "customcenter/helpContent.jsp";
 	}
 
--- src/com/helpComment/action/commentWrite.java
+++ src/com/helpComment/action/commentWrite.java
@@ -1,4 +1,4 @@
-package com.customcenter.action;
+package com.helpComment.action;
 
 import java.io.IOException;
 
@@ -7,32 +7,33 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.command.CommandHandler;
-import com.help.db.helpBean;
-import com.help.db.helpDAO;
+import com.helpComment.db.helpCommentBean;
+import com.helpComment.db.helpCommentDAO;
 
-public class helpWriteAction implements CommandHandler{
+public class commentWrite implements CommandHandler{
 
 	@Override
 	public String process(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
+		
 		String id = "";
 		if(request.getSession().getAttribute("userid") != null){
 			id = (String)request.getSession().getAttribute("userid");
 		}else if(request.getSession().getAttribute("storeid") != null){
 			id = (String)request.getSession().getAttribute("storeid");
 		}
-		String title = request.getParameter("title");
-		String content = request.getParameter("content");
-		helpBean bean = new helpBean();
-		bean.setTitle(title);
-		bean.setContent(content);
-		bean.setWriter(id);
-		helpDAO dao = new helpDAO();
-		int check = dao.insertHelp(bean);
-		System.out.println("check : " + check);
-		request.setAttribute("data",1);
-		System.out.println("data : " + request.getAttribute("data"));
+		
+		String comment = request.getParameter("comment");
+		int num = Integer.parseInt(request.getParameter("num"));
+		
+		helpCommentDAO dao = new helpCommentDAO();
+		helpCommentBean bean = new helpCommentBean();
+		bean.setId(id);
+		bean.setComment(comment);
+		bean.setNum(num);
+		int check = dao.insertComment(bean);
+		request.setAttribute("data", check);
 		return "store/checkEmail.jsp";
 	}
 
